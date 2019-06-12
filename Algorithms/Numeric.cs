74892a920d39a48a77352385c4986820620efa4e
Alexey <Mr.HofD@gmail.com>
2019-06-12 12:33:13 +0300

    Gcd and rand array

---
+++ Algorithms/Numeric.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class Numeric
+    {
+        /// <summary>
+        /// Нахождение наибольшего общего делителя (Алгоритм Евклида)
+        /// </summary>
+        /// <param name="a">Число а</param>
+        /// <param name="b">Число b</param>
+        /// <returns>НОД</returns>
+        public static int Gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+
+            return a;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        public static void RandomizeArray(int[] array)
+        {
+            int max_i = array.Length;
+            Random rand = new Random();
+
+            for (int i = 0; i < max_i - 1; i++)
+            {
+                int j = rand.Next(i, max_i);
+                int t = array[j];
+                array[j] = array[i];
+                array[i] = t;
+            }
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,7 +6,13 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Find GCD for 233 and 23");
+            Console.WriteLine($"Result is: {Numeric.Gcd(233, 23)}");
+
+            Console.WriteLine("Randomize array 0 1 2 3 4 5 6 7 8 9");
+            int[] randArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Numeric.RandomizeArray(randArray);
+            Console.WriteLine($"Result is: {randArray[0]} {randArray[1]} {randArray[2]} {randArray[3]} {randArray[4]} {randArray[5]} {randArray[6]} {randArray[7]} {randArray[8]} {randArray[9]}");
         }
     }
 }
